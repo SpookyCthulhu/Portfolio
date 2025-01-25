7fe90ef0b301586b296a85c20b369001e24db70d
Michelle <161183828+SpookyCthulhu@users.noreply.github.com>
2025-01-25 05:18:11 -0500

    Testing Deployment VIII

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    // For GitHub Pages deployments under a subpath (e.g., your-repo-name/)
-    basePath: process.env.NODE_ENV === "production" ? "/Portfolio" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/Portfolio/" : "",
-  };
-  
-  export default nextConfig;
-  
+  output: "export",
+};
+
+export default nextConfig;
