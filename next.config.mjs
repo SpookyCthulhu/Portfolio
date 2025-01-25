98613bddd75d514bb4d679671a69064ee7013aef
Michelle <161183828+SpookyCthulhu@users.noreply.github.com>
2025-01-25 15:54:17 -0500

    Removed repo name from path

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: "export",
     // For GitHub Pages deployments under a subpath (e.g., your-repo-name/)
-    basePath: process.env.NODE_ENV === "production" ? "/Portfolio" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/Portfolio/" : "",
+    // basePath: process.env.NODE_ENV === "production" ? "/Portfolio" : "",
+    // assetPrefix: process.env.NODE_ENV === "production" ? "/Portfolio/" : "",
   };
   
   export default nextConfig;
